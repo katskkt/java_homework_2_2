00357387e1a4ae1afa9b4274189657ea2a76a156
Ekaterina <katskktt@gmail.com>
2026-02-18 12:03:04 +0300

    Modernization bonus

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int initialBalance = 100;
-        int depozitAmount = 1200;
-        int bonusThreshold = 1000;
-        int bonus = 100;
-        int bonusRubles;
-        int finalBalance;
-
-
-        if (depozitAmount > bonusThreshold) {
-            bonusRubles = depozitAmount / bonus;
-        } else {
-            bonusRubles = 0;
-        }
-
-        finalBalance = initialBalance + depozitAmount + bonusRubles;
-
-        System.out.println("Итоговая сумма на счету: " + finalBalance + " руб.");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
-
 }
